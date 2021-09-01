03163d398984727141366d4978b63bceea15ee33
Sergey Maslov <maslovss@gmail.com>
2021-09-01 21:06:55 +0300

    fix14

---
+++ network.tf
@@ -0,0 +1,35 @@
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-${var.rgname}"
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "vnets" {
+  for_each            = var.vnets
+
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+  name                = "${var.prefix}-${each.key}"
+  address_space       = each.value.cidr_blocks
+
+   dynamic "subnet" {
+      for_each = each.value.subnet_address
+      content {
+        name           = "${var.prefix}-subnet-${each.key}-${subnet.key}"
+        address_prefix = subnet.value
+      }
+   }
+
+  tags = var.tags
+
+}
+
+resource "azurerm_virtual_network_peering" "peering" {
+  for_each = var.peering    
+  name                      = "${var.prefix}-peer-${each.value.from}-to-${each.value.to}"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = lookup(azurerm_virtual_network.vnets , each.value.from).name
+  remote_virtual_network_id = lookup(azurerm_virtual_network.vnets , each.value.to).id
+}

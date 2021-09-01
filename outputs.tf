54eebe1aae37646d9d518fcb5a046a250848bb74
Sergey Maslov <maslovss@gmail.com>
2021-09-02 02:04:14 +0300

    fix36

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "tls_private_key" {
     value = {for k, v in  tls_private_key.ssh : k => v.private_key_pem }
     sensitive  = true
 }
+
+
+output "public_ips" {
+    value = {for k, v in  azurerm_public_ip.publicip : k => v.ip_address }
+}
